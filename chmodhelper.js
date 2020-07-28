3014ac2571805f815ea679611707eeddff7a5893
Pedro Sanders <sanderspedro@gmail.com>
2020-07-27 20:44:58 -0400

    Refactored

--- chmodhelper.js
+++ chmodhelper.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
-const chokidar = require('chokidar')
+const EventsRecvr = require('@fonos/events').EventsRecvr
+const logger = require('@fonos/logger').default
 const fs = require('fs')
+const { exit } = require('process')
 
-chokidar.watch('/data', { ignoreInitial: true }).on('add', path => {
+if (!process.env.EVENTS_BROKERS) {
+  logger.log('error', 'chmodhelper [environment variable EVENTS_BROKERS is undefined]')
+  exit(1)
+}
+const BROCKERS = process.env.EVENTS_BROKERS.split(',')
+const er = new EventsRecvr(BROCKERS, process.env.EVENTS_QUEUE)
+er.connect()
+
+er.watchEvents(content => {
+  logger.log(`chmodhelper received new event [payload => ${content.toString()}]`)
+  const event = JSON.parse(content.toString())
+  if (event.name === 'APP_CREATED') {
+    console.log(`chmodhelper received new event [payload => ${content.toString()}]`)
+    logger.error(`chmodhelper received new event [payload => ${content.toString()}]`)
+  }
+})
+
+/*chokidar.watch('/data', { ignoreInitial: true }).on('add', path => {
   console.log(`File ${path} has been added. Changing mode to 0755`)
   // TODO: Make this a parameter
   if (path.endsWith('.js')) fs.chmodSync(path, 0755)
-})
+})*/
