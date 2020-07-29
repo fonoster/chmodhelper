f79d016f0d32a98aa534f6b2ba58dd05440bfcf4
Pedro Sanders <sanderspedro@gmail.com>
2020-07-29 07:56:11 -0400

    Update chmodhelper.js

--- chmodhelper.js
+++ chmodhelper.js
@@ -4,7 +4,7 @@ const logger = require('@fonos/logger').default
 const fs = require('fs')
 const { join } = require('path')
 const { exit } = require('process')
-const BASE_DIR = process.env.BASE_DIR || '/data'
+const BASE_DIR = process.env.BASE_DIR || '/data/apps'
 
 if (!process.env.EVENTS_BROKERS) {
   logger.log('error', 'chmodhelper [environment variable EVENTS_BROKERS is undefined]')
@@ -19,25 +19,22 @@ er.watchEvents(content => {
   const event = JSON.parse(content.toString())
   if (event.name === 'APP_CREATED') {
     const pathToPackage = join(BASE_DIR, event.data.name, 'package.json')
-    let pathToEntryPoint 
+    let pathToEntryPoint
+
+    logger.log('debug', `chmodhelper received new event [pathToPackage => ${pathToPackage}]`)
 
     try {
       const entryPoint = require(pathToPackage).main
       pathToEntryPoint = join(BASE_DIR, event.data.name, entryPoint)
     } catch(e) {
-    }
-
-    // Go for main first
-    if (!fs.exists(pathToEntryPoint)) {
-      // If no main does not exist then try index.js
       pathToEntryPoint = join(BASE_DIR, event.data.name, 'index.js')
     }
 
-    if (fs.exists(pathToEntryPoint)) {
-      fs.chmodSync(pathToEntryPoint, 0755)
+    if (fs.existsSync(pathToEntryPoint)) {
+      fs.chmodSync(pathToEntryPoint, '0755')
       logger.log('debug', `chmodhelper [changed file ${pathToEntryPoint} mode to 0755]`)
     } else {
       logger.log('debug', `chmodhelper [unable to find entrypoint]`)
-    }    
+    }
   }
 })
